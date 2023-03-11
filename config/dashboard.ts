cbe37b119d360263b564c103108b1e6ffce4217c
Brian Jenkins <brian@jenkinz.com>
2023-03-11 12:18:45 -0800

    Update logged in navigation

--- config/dashboard.ts
+++ config/dashboard.ts
@@ -2,27 +2,32 @@ import { DashboardConfig } from "types"
 
 export const dashboardConfig: DashboardConfig = {
   mainNav: [
-    {
-      title: "Documentation",
-      href: "/docs",
-    },
-    {
-      title: "Support",
-      href: "/support",
-      disabled: true,
-    },
+    // {
+    //   title: "Documentation",
+    //   href: "/docs",
+    // },
+    // {
+    //   title: "Support",
+    //   href: "/support",
+    //   disabled: true,
+    // },
   ],
   sidebarNav: [
     {
-      title: "Posts",
-      href: "/dashboard",
-      icon: "post",
-    },
-    {
-      title: "Billing",
-      href: "/dashboard/billing",
+      title: "My Contributions",
+      href: "/contributions",
       icon: "billing",
     },
+    {
+      title: "My Signed Letters",
+      href: "/signed-letters",
+      icon: "post",
+    },
+    // {
+    //   title: "Billing",
+    //   href: "/dashboard/billing",
+    //   icon: "billing",
+    // },
     {
       title: "Settings",
       href: "/dashboard/settings",
--- config/site.ts
+++ config/site.ts
@@ -1,13 +1,12 @@
 import { SiteConfig } from "types"
 
 export const siteConfig: SiteConfig = {
-  name: "Taxonomy",
-  description:
-    "An open source application built using the new router, server components and everything new in Next.js 13.",
-  url: "https://tx.shadcn.com",
-  ogImage: "https://tx.shadcn.com/og.jpg",
+  name: "Govern For California Supporter",
+  description: "Govern For California supporter portal.",
+  url: "https://supporter.governforcalifornia.org",
+  ogImage: "https://www.governforcalifornia.org/gfc-og.png",
   links: {
-    twitter: "https://twitter.com/shadcn",
-    github: "https://github.com/shadcn/taxonomy",
+    twitter: "https://twitter.com/govern4ca",
+    github: "https://github.com/jenkinz/taxonomy",
   },
 }
